1e0db9bbba7cfd4c711f70a787b36f3080e099eb
Lucas Gomes <lulinux2109@gmail.com>
2025-05-06 00:46:41 -0300

    feat: add a page prop drilling

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,13 @@
-import { useState } from 'react'
+import { PropDrilling } from './pages/PropDrilling'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-
-      <h1>Contador</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          + 1
-        </button>
-
-        <Nivel1 value={count} />
-
-        <button onClick={() => setCount((count) => count - 1)}>
-          - 1
-        </button>
-      </div>
-
+      <PropDrilling />
     </>
   )
 }
 
 export default App
-
-function Nivel1(props: { value: number}) {
-  return (
-    <>
-      <Nivel2 value={props.value} />
-    </>
-  )
-}
-
-function Nivel2(props: { value: number}) {
-  return (
-    <>
-      <Nivel3 value={props.value} />
-    </>
-  )
-}
-
-function Nivel3(props: { value: number}) {
-  return (
-    <>
-      {props.value}
-    </>
-  )
-}
