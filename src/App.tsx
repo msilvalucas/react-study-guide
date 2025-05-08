0f232e3c03ae2a1efba9cecad7e0ec75da9dd4b3
Lucas Gomes <lulinux2109@gmail.com>
2025-05-07 23:56:01 -0300

    :sparkles: feat: uses context to share data between components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
-import { PropDrilling } from './pages/PropDrilling'
+import { useContext, useState } from 'react'
+
+import { CountContext } from './pages/Context'
+
+// import { PropDrilling } from './pages/PropDrilling'
 
 import './App.css'
 
+
 function App() {
+  const [count, setCount] = useState(0)
   return (
-    <>
-      <PropDrilling />
-    </>
+    <CountContext.Provider value={{ count, setCount }}>
+      <Nivel1 />
+    </CountContext.Provider>
   )
 }
 
 export default App
+
+function Nivel1() {
+  return (
+    <>
+      <h2>Eu sou o nível 1</h2>
+      <Nivel2 />
+    </>
+  )
+}
+
+function Nivel2() {
+  const { count } = useContext(CountContext)!
+  return (
+    <h2>Valor do count: {count}</h2>
+  )
+}
